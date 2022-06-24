074cde235d902d6a7d209e3091795e53ee7669d8
victorfeijoo <100071272+victorfeijoo@users.noreply.github.com>
2022-06-24 12:56:35 +0200

    Add files via upload

---
+++ src/Pieza.cpp
@@ -0,0 +1,5 @@
+#include "Pieza.h"
+
+Pieza::Pieza() {
+
+}
---
+++ src/Tablero.cpp
@@ -0,0 +1,17 @@
+#include "Tablero.h"
+
+Tablero::Tablero() {
+
+}
+
+void Tablero::imprimir() {
+
+}
+
+void Tablero::moverPieza(Pos pos_inicial, Pos pos_final) {
+	//actualizar posición de cierta pieza
+}
+
+void Tablero::quitarPieza(Pos pos) {
+	//hacer null el puntero a la pieza deseada, hacer "0" la posición de esa pieza (suponiendo que la pieza tuviera un atributo pos)
+}
